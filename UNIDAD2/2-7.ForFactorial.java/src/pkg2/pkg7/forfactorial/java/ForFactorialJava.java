ce1f9ddcc3afd72f8db6ee5907220b66e7faca9a
Vania Dafne De Leon Angeles <133055276+DafneDeLeon@users.noreply.github.com>
2023-06-30 12:30:18 -0600

    Se agrega ForFactorial

---
+++ UNIDAD2/2-7.ForFactorial.java/src/pkg2/pkg7/forfactorial/java/ForFactorialJava.java
@@ -0,0 +1,23 @@
+package pkg2.pkg7.forfactorial.java;
+import java.util.Scanner;
+/**
+ *
+ * @author Dafne
+ */
+public class ForFactorialJava {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double factorial;
+        int num;
+        System.out.println("Introduce el numero: ");
+        num = entrada.nextInt();
+        factorial = 1;
+        for(int i = num; i > 0; i--){
+            factorial = factorial*i;
+        }
+        System.out.println("El factorial de " + num + "es: "+ factorial);
+        
+    }
+    
+}
